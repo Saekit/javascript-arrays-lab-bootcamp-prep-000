01d8ad26265c3a29516d882851d7d16f65bedb84
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-05 22:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  return
+  return kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
 }
